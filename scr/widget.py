4bc70f4f4af0eb66c0d7f0df03c84261b0dc9b6c
marina grebneva <grmarcw@gmail.com>
2025-02-21 23:38:55 +0500

    add func mask_account_card and get_date

---
+++ scr/widget.py
@@ -0,0 +1,28 @@
+def mask_account_card(type_of_card_and_number_of_card: str) -> str:
+    import masks
+    """обрабатывает информацию о картах и счетах"""
+    type_of_card = []
+    number_of_card = []
+
+    for element in type_of_card_and_number_of_card:
+        if element.isdigit():
+            number_of_card.append(element)
+        else:
+            type_of_card.append(element)
+
+    join_type = ''.join(type_of_card)
+    join_number = ''.join(number_of_card)
+
+    if join_type == 'Счет ':
+        return f'{join_type} {masks.get_mask_account(join_number)}'
+    else:
+        return f'{join_type} {masks.get_mask_card_number(join_number)}'
+
+def get_date(iso_str: str) -> str:
+    """принимает формат iso(str), возвращает формат 'ДД.ММ.ГГГГ'(str)"""
+    from datetime import datetime
+
+    date_iso_object = datetime.fromisoformat(iso_str)
+    date_obj = date_iso_object.strftime('%d%m%Y')
+
+    return f'{date_obj[:2]}.{date_obj[2:4]}.{date_obj[4:]}'
