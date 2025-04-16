5f9f12cd0c2cc2c0da5a63d5ad2c47a48f41c660
marina grebneva <grmarcw@gmail.com>
2025-04-16 22:30:40 +0500

    add decorator 'log'

---
+++ scr/decorators.py
@@ -0,0 +1,29 @@
+def log(filename=None):
+    def decorator(function):
+        def inner(*args, **kwargs):
+            start = f'Начало работы функции {function.__name__}'
+            finish = f"Работа функции {function.__name__} завершена"
+            try:
+                func = function(*args, **kwargs)
+                result = f"Результат работы функции: {func}"
+                if filename is not None:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(start + "\n")
+                        file.write(result + "\n")
+                        file.write(finish + "\n")
+                else:
+                    return f"{start}\n{result}\n{finish}"
+            except Exception as e:
+                if filename is not None:
+                    error = f"ошибка: {e}"
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(start + "\n")
+                        file.write(error + "\n")
+                        file.write(finish + "\n")
+                else:
+                    return f"{start}\nошибка: {e}\n{finish}"
+
+        return inner
+
+    return decorator
+
